49890409e0a818fe9cd45d4e28e8f1adb4aa7793
Justin Refi <refi@singularcuriosity.com>
2018-01-17 17:59:48 -0600

    Update aws config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,7 +2,9 @@ if ENV['AWS_ACCESS_KEY_ID'] then
   Aws.config.update({
     credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
     endpoint: "http://#{ENV['S3_HOST_NAME']}",
-    force_path_style: true
+    region: 'us-east-1',
+    force_path_style: true,
+    signature_version: "v4"
   })
 
   S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
